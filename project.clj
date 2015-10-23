24644fa32c4a68911be958010db8275973345f02
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-23 16:20:18 +0100

    Move code from photon

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject tranchis/photon-cassandra "0.9.9"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [tranchis/photon-db "0.9.9"]
+                 [tranchis/photon-config "0.9.8"]
+                 [tranchis/cassaforte "2.1.0-beta3"]
+                 [cheshire "5.5.0"]
+                 [dire "0.5.3"]
+                 [org.clojure/tools.logging "0.3.1"]])
+
